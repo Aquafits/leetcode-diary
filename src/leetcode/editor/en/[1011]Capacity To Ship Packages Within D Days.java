08bb1e3068dfd55912ef41239cbb6bf971608598
Aquafits <aquafits@outlook.com>
2022-02-07 01:39:15 -0800

    add: 763, 1011

---
+++ src/leetcode/editor/en/[1011]Capacity To Ship Packages Within D Days.java
@@ -0,0 +1,96 @@
+//A conveyor belt has packages that must be shipped from one port to another 
+//within days days. 
+//
+// The iᵗʰ package on the conveyor belt has a weight of weights[i]. Each day, 
+//we load the ship with packages on the conveyor belt (in the order given by 
+//weights). We may not load more weight than the maximum weight capacity of the ship. 
+//
+// Return the least weight capacity of the ship that will result in all the 
+//packages on the conveyor belt being shipped within days days. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: weights = [1,2,3,4,5,6,7,8,9,10], days = 5
+//Output: 15
+//Explanation: A ship capacity of 15 is the minimum to ship all the packages in 
+//5 days like this:
+//1st day: 1, 2, 3, 4, 5
+//2nd day: 6, 7
+//3rd day: 8
+//4th day: 9
+//5th day: 10
+//
+//Note that the cargo must be shipped in the order given, so using a ship of 
+//capacity 14 and splitting the packages into parts like (2, 3, 4, 5), (1, 6, 7), (8)
+//, (9), (10) is not allowed.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: weights = [3,2,2,4,1,4], days = 3
+//Output: 6
+//Explanation: A ship capacity of 6 is the minimum to ship all the packages in 3
+// days like this:
+//1st day: 3, 2
+//2nd day: 2, 4
+//3rd day: 1, 4
+// 
+//
+// Example 3: 
+//
+// 
+//Input: weights = [1,2,3,1,1], days = 4
+//Output: 3
+//Explanation:
+//1st day: 1
+//2nd day: 2
+//3rd day: 3
+//4th day: 1, 1
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= days <= weights.length <= 5 * 10⁴ 
+// 1 <= weights[i] <= 500 
+// Related Topics Array Binary Search Greedy 👍 3644 👎 84
+
+
+import java.util.Arrays;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int l = weights[weights.length - 1], r = 0;
+        for (int w : weights) r += w;
+
+        while (l < r) {
+            int mid = (l + r) >> 1;
+            if (shippable(weights, mid, days)) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+
+    private boolean shippable(int[] weights, int capacity, int days) {
+        int i = 0, cur = 0;
+        while (i < weights.length && days > 0) {
+            while (i < weights.length && cur + weights[i] <= capacity) {
+                cur += weights[i];
+                i++;
+            }
+            days--;
+            cur = 0;
+        }
+
+        return i == weights.length;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ src/leetcode/editor/en/[763]Partition Labels.java
@@ -0,0 +1,65 @@
+//You are given a string s. We want to partition the string into as many parts 
+//as possible so that each letter appears in at most one part. 
+//
+// Note that the partition is done so that after concatenating all the parts in 
+//order, the resultant string should be s. 
+//
+// Return a list of integers representing the size of these parts. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: s = "ababcbacadefegdehijhklij"
+//Output: [9,7,8]
+//Explanation:
+//The partition is "ababcbaca", "defegde", "hijhklij".
+//This is a partition so that each letter appears in at most one part.
+//A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it 
+//splits s into less parts.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: s = "eccbbbbdec"
+//Output: [10]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= s.length <= 500 
+// s consists of lowercase English letters. 
+// 
+// Related Topics Hash Table Two Pointers String Greedy 👍 6284 👎 251
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        char[] cs = s.toCharArray();
+        int N = cs.length;
+        int[] right = new int[26];
+        for (int i = 0; i < N; i++) {
+            right[cs[i] - 'a'] = i;
+        }
+
+        List<Integer> res = new ArrayList<>();
+        int l = 0;
+        while (l < N) {
+            int i = l, r = right[cs[i] - 'a'];
+            while (i < r) {
+                r = Math.max(r, right[cs[++i] - 'a']);
+            }
+            res.add(r - l + 1);
+            l = i + 1;
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
